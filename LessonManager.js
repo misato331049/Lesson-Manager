245e2ab0da0943c461a19663e3c05bdea2b3f90a
misato331049 <misato331049@gmail.com>
2026-06-07 23:57:19 +0900

    Add files via upload

---
+++ LessonManager.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'YOUR_SUPABASE_URL'       // ← SupabaseのProject URL
+const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'  // ← Supabaseのanon key
+const TEACHER_PASSWORD = 'Lesson2026'          // ← 任意の講師パスワードに変更
